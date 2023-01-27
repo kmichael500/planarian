8499b3cd6dcb3e2a284c842217415783acfa46cd
Michael Ketzner <michaelketzner@gmail.com>
2023-01-27 12:46:56 -0600

    front end seems to work

--- Planarian.Web/src/Modules/Objective/Models/CreateOrEditTripObjectiveVm.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateOrEditTripObjectiveVm {
-  id: string | null;
-  projectId: string;
-  name: string;
-  description: string;
-  tripReport: string | null;
-  tripObjectiveTypeIds: string[];
-  tripObjectiveMemberIds: string[];
-}
---
+++ Planarian.Web/src/Shared/Components/TripTagComponent.tsx
@@ -0,0 +1,37 @@
+import { Spin, Tag } from "antd";
+import { PresetColorType, PresetStatusColorType } from "antd/es/_util/colors";
+import { LiteralUnion } from "antd/es/_util/type";
+import { useState, useEffect } from "react";
+import { SettingsService } from "../../Modules/Settings/Services/settings.service";
+
+export interface TripTagComponentProps {
+  tagId: string;
+  color?: LiteralUnion<PresetColorType | PresetStatusColorType, string>;
+}
+const TripTagComponent: React.FC<TripTagComponentProps> = (props) => {
+  let [tagName, setTagName] = useState<string>();
+  let [isTagNameLoading, setIsTagNameLoading] = useState(true);
+
+  useEffect(() => {
+    if (tagName === undefined) {
+      const getTagName = async () => {
+        const tripObjectiveNameResponse = await SettingsService.GetTagName(
+          props.tagId
+        );
+        setTagName(tripObjectiveNameResponse);
+        setIsTagNameLoading(false);
+      };
+      getTagName();
+    }
+  });
+
+  return (
+    <Spin spinning={isTagNameLoading}>
+      <Tag key={props.tagId} color={props.color}>
+        {tagName}
+      </Tag>
+    </Spin>
+  );
+};
+
+export { TripTagComponent };
