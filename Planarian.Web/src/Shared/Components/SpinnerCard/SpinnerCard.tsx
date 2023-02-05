febaed58c5b7487d907fe8f8e1e9f0afb5e27e39
Michael Ketzner <michaelketzner@gmail.com>
2023-02-05 08:49:19 -0600

    replaced spinner with spinning cards

---
+++ Planarian.Web/src/Shared/Components/SpinnerCard/SpinnerCard.tsx
@@ -0,0 +1,41 @@
+import { Card } from "antd";
+import { CardGridComponent } from "../CardGrid/CardGridComponent";
+
+interface SpinnerCardProps {
+  children?: React.ReactNode;
+  className?: string;
+  spinning: boolean;
+  numberOfCards?: number;
+}
+
+const SpinnerCardComponent: React.FC<SpinnerCardProps> = ({
+  spinning,
+  children,
+  className,
+  numberOfCards = 8,
+  ...props
+}) => {
+  return (
+    <>
+      {spinning && (
+        <CardGridComponent
+          items={[...Array(numberOfCards).keys()].map((_, index) => {
+            return (
+              <>
+                <Card
+                  key={index}
+                  loading={spinning}
+                  className={className}
+                  {...props}
+                />
+              </>
+            );
+          })}
+        />
+      )}
+      {!spinning && children}
+    </>
+  );
+};
+
+export { SpinnerCardComponent };
