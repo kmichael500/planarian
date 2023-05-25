66b543208808b82d2aa28562a67b5790eb408056
Michael Ketzner <michaelketzner@gmail.com>
2023-05-24 22:53:51 -0500

    database

--- Planarian/Planarian.Model/Database/Entities/TagType.cs
+++ Planarian/Planarian.Model/Database/Entities/TagType.cs
@@ -24,6 +24,15 @@ public class TagType : EntityBaseNameId
     public virtual ICollection<Trip> Trips { get; set; } = new HashSet<Trip>();
     public virtual ICollection<TripTag> TripTags { get; set; } = new HashSet<TripTag>();
     public virtual ICollection<LeadTag> LeadTags { get; set; } = new HashSet<LeadTag>();
+    public virtual ICollection<EntranceStatusTag> EntranceStatusTags { get; set; } = new HashSet<EntranceStatusTag>();
+
+    public virtual ICollection<EntranceHydrologyTag> EntranceHydrologyTags { get; set; } =
+        new HashSet<EntranceHydrologyTag>();
+    
+    public virtual ICollection<EntranceHydrologyFrequencyTag> EntranceHydrologyFrequencyTags { get; set; } =
+        new HashSet<EntranceHydrologyFrequencyTag>();
+    public virtual ICollection<FieldIndicationTag> FieldIndicationTags { get; set; } = new HashSet<FieldIndicationTag>();
+    public ICollection<Entrance> EntranceLocationQualities { get; set; } = new HashSet<Entrance>();
     public virtual Project? Project { get; set; }
 }
 
