14746ea76b0684319d3d335d9281d35713fbeb5e
Michael Ketzner <michaelketzner@gmail.com>
2023-06-16 12:06:21 -0500

    file db changes

---
+++ Planarian/Planarian.Migrations/Migrations/20230616170042_v12.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Planarian.Migrations.Migrations
+{
+    public partial class v12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Maps");
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    FileTypeTagId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    CaveId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    AccountId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    CreatedByUserId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ModifiedByUserId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Files_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Files_Caves_CaveId",
+                        column: x => x.CaveId,
+                        principalTable: "Caves",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Files_TagTypes_FileTypeTagId",
+                        column: x => x.FileTypeTagId,
+                        principalTable: "TagTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_AccountId",
+                table: "Files",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_CaveId",
+                table: "Files",
+                column: "CaveId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_FileTypeTagId",
+                table: "Files",
+                column: "FileTypeTagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.CreateTable(
+                name: "Maps",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    CaveId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    MapStatusTagId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    CreatedByUserId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedByUserId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Maps_Caves_CaveId",
+                        column: x => x.CaveId,
+                        principalTable: "Caves",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Maps_TagTypes_MapStatusTagId",
+                        column: x => x.MapStatusTagId,
+                        principalTable: "TagTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maps_CaveId",
+                table: "Maps",
+                column: "CaveId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maps_MapStatusTagId",
+                table: "Maps",
+                column: "MapStatusTagId");
+        }
+    }
+}
