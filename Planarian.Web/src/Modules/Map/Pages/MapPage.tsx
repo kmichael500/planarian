1dc7f6053135529121ef84cd1cd3753bdd04adf5
Michael Ketzner <michaelketzner@gmail.com>
2025-03-23 17:22:12 -0500

    Map UI Improvements (#205)

--- Planarian.Web/src/Modules/Map/Pages/MapPage.tsx
+++ Planarian.Web/src/Modules/Map/Pages/MapPage.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import { AppContext } from "../../../Configuration/Context/AppContext";
 import { MapComponent } from "../Components/MapComponent";
+import { useLocation } from "react-router-dom";
 
 const MapPage = () => {
   const {
@@ -9,6 +10,18 @@ const MapPage = () => {
     setContentStyle,
     defaultContentStyle,
   } = useContext(AppContext);
+
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const lat = params.get("lat");
+  const lng = params.get("lng");
+  const zoomParam = params.get("zoom");
+
+  const initialCenter =
+    lat && lng
+      ? ([parseFloat(lat), parseFloat(lng)] as [number, number])
+      : undefined;
+  const initialZoom = zoomParam ? parseFloat(zoomParam) : undefined;
 
   useEffect(() => {
     setHeaderTitle([`Map`]);
@@ -22,7 +35,23 @@ const MapPage = () => {
 
   return (
     <>
-      <MapComponent />
+      <MapComponent
+        initialCenter={initialCenter}
+        initialZoom={initialZoom}
+        onMoveEnd={(event) => {
+          const { latitude, longitude, zoom } = event.viewState;
+
+          const searchParams = new URLSearchParams(window.location.search);
+          searchParams.set("lat", latitude.toString());
+          searchParams.set("lng", longitude.toString());
+          searchParams.set("zoom", zoom.toString());
+          window.history.replaceState(
+            null,
+            "",
+            window.location.pathname + "?" + searchParams.toString()
+          );
+        }}
+      />
     </>
   );
 };
---
+++ Planarian.Web/src/Shared/Services/NavigationService.ts
@@ -0,0 +1,29 @@
+import { NavigateFunction } from "react-router-dom";
+import { isNullOrWhiteSpace } from "../Helpers/StringHelpers";
+
+const NavigationService = {
+  NavigateToMap(
+    latitude: number,
+    longittude: number,
+    zoom: number,
+    navigate: NavigateFunction
+  ) {
+    const url = NavigationService.GenerateMapUrl(latitude, longittude, zoom);
+    navigate(url);
+  },
+  GenerateMapUrl(latitude?: number, longittude?: number, zoom?: number) {
+    const searchParams = new URLSearchParams();
+    if (latitude) {
+      searchParams.set("lat", latitude.toString());
+    }
+    if (longittude) {
+      searchParams.set("lng", longittude.toString());
+    }
+    if (zoom) {
+      searchParams.set("zoom", zoom.toString());
+    }
+    const queryString = searchParams.toString();
+    return `/map${!isNullOrWhiteSpace(queryString) ? `?${queryString}` : ""}`;
+  },
+};
+export { NavigationService };
