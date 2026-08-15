613d254ad1a57ff373ffabca181cfd8867a62e3b
Michael Ketzner <michaelketzner@gmail.com>
2026-08-15 18:38:25 -0500

    added csp

--- Planarian.Web/src/index.tsx
+++ Planarian.Web/src/index.tsx
@@ -4,6 +4,7 @@ import utc from "dayjs/plugin/utc";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import dayjs from "dayjs";
 import { registerLicense } from "@syncfusion/ej2-base";
+import "maplibre-gl/dist/maplibre-gl.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
